a1c6c9361cb28309763f22b1e7b6c942e5269133
Rionzo <101568609+Rionzo@users.noreply.github.com>
2022-11-17 15:50:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const modalContainer = document.querySelector(".modal-container");
+
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach((trigger) =>
+  trigger.addEventListener("click", toggleModal)
+);
+
+function toggleModal() {
+  modalContainer.classList.toggle("active");
+}
+
+
+
